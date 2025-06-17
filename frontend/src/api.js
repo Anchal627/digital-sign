f617477aa5bccd2a8c55514329b7e07a9500e4b1
Anchal627 <140655663+Anchal627@users.noreply.github.com>
2025-06-17 20:20:07 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://digital-sign.onrender.com/api";
 // User Authentication
 export const registerUser = async (userData) => {
   console.log(userData);
